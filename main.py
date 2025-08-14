99a474dc28e608b14db2561b47cc63574eb13f97
SajaniBandara <149466419+SajaniBandara@users.noreply.github.com>
2025-08-14 14:15:39 +0530

    create  the file stuctured

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/station2')
+def station2():
+    return render_template('station2.html')
+
+@app.route('/station3')
+def station3():
+    return render_template('station3.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
